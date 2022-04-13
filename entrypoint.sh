7f9264afd638ff22bc47ff62922b23e8d917a262
imspace <spacemeowx2@gmail.com>
2022-04-13 09:46:48 +0000

    fix: entrypoint args

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,17 @@ LIBNX_REV="$3"
 
 # if LIBNX_REV is exist, update libnx
 if [ -n "${LIBNX_REV}" ]; then
-git clone -b ${LIBNX_REV} https://github.com/switchbrew/libnx.git --depth=1 \
-    && cd libnx \
-    && make -j8 \
-    && make install \
-    && cd .. \
-    && rm -rf libnx
+    echo "Update libnx to ${LIBNX_REV}"
+    git clone https://github.com/switchbrew/libnx.git \
+        && cd libnx \
+        && git reset --hard ${LIBNX_REV} \
+        && make -j8 \
+        && make install \
+        && cd .. \
+        && rm -rf libnx
 elif [ "$UPDATE_LIBNX" = "1" ]; then
+    echo "Update libnx by dpk-pacman"
     dkp-pacman -Sy --noconfirm libnx
 fi;
 
-$2
+$CMD
